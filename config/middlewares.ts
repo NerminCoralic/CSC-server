e0d1e766f95ad9c7ffa21c39c1e342fd39770387
Nermin Coralic <nermin.coralic.ets@gmail.com>
2025-02-23 12:13:53 +0100

    Railway testing

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,30 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "default-src": ["'self'"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            process.env.SUPABASE_URL,
+          ],
+          "media-src": ["'self'", "data:", process.env.SUPABASE_URL],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
