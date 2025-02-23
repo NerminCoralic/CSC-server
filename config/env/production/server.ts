4ed6c9fdb4304ffb3248238ccd849005e38cf3fe
Nermin Coralic <nermin.coralic.ets@gmail.com>
2025-02-23 19:22:37 +0100

    Fix: Added PORT and HOST for Railway deploy

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,7 +1,8 @@
-// path: /config/env/production/server.ts
 export default ({ env }) => ({
   proxy: true,
   url: env("APP_URL"), // Sets the public URL of the application.
+  host: "0.0.0.0",
+  port: env.int("PORT", process.env.PORT || 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
