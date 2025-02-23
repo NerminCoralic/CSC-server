993989fa9a4b6e488c4d8c78e222ea63bca06b41
Nermin Coralic <nermin.coralic.ets@gmail.com>
2025-02-23 19:41:49 +0100

    Middleware configuration & Plugin code delete

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,26 +1,6 @@
 export default [
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "default-src": ["'self'"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            process.env.SUPABASE_URL,
-          ],
-          "media-src": ["'self'", "data:", process.env.SUPABASE_URL],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  "strapi::security",
   "strapi::cors",
   "strapi::query",
   "strapi::body",
