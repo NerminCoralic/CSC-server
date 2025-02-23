35edbb312970346b4ac579539c81e68b4ca2804d
Nermin Coralic <nermin.coralic.ets@gmail.com>
2025-02-23 17:59:01 +0100

    Production ready, depolyment setup

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,16 +1,15 @@
-module.exports = ({ env }) => ({
+// path: /config/env/production/database.ts
+export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: {
-        ca: env("DATABASE_CA"),
-      },
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5432),
+      database: env("PGDATABASE", "strapi"),
+      user: env("PGUSER", "strapi"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(true),
     },
-    debug: false,
+    pool: { min: 0 },
   },
 });
