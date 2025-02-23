31b6370a17c8e5bd1fee3735d7e3d219c8fbe6d9
Nermin Coralic <nermin.coralic.ets@gmail.com>
2025-02-23 20:06:34 +0100

    Plugin modified

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -1,32 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = [
-    "strapi::errors",
-    {
-        name: "strapi::security",
-        config: {
-            contentSecurityPolicy: {
-                useDefaults: true,
-                directives: {
-                    "connect-src": ["'self'", "https:"],
-                    "default-src": ["'self'"],
-                    "img-src": [
-                        "'self'",
-                        "data:",
-                        "blob:",
-                        "market-assets.strapi.io",
-                        process.env.SUPABASE_URL,
-                    ],
-                    "media-src": ["'self'", "data:", process.env.SUPABASE_URL],
-                    upgradeInsecureRequests: null,
-                },
-            },
-        },
-    },
-    "strapi::cors",
-    "strapi::query",
-    "strapi::body",
-    "strapi::session",
-    "strapi::favicon",
-    "strapi::public",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
